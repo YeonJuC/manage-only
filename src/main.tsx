b01665ffe3de73147916040ccea9c7d901216b4a
carplay1110 <carplay1110@naver.com>
2026-01-28 16:58:14 +0900

    fix: router + tasks ui polish

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+// src/main.tsx
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
