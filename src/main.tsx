b7986cbc2f3abb84e48dba440b4bfa13e63affa7
carplay1110 <carplay1110@naver.com>
2026-01-29 10:12:50 +0900

    화면 수정 중

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-console.log("DEPLOY CHECK: HASHROUTER ON"); // 배포 확인용
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
